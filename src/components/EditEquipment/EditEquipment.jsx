adf4237feb90bfe87d7b73b8ad8775a9b84bb40b
encoder-shoaib <ahammed-15-4955@diu.edu.bd>
2025-05-07 17:08:18 +0600

    update EditEquipment and MyEquipmentList page for correction

--- src/components/EditEquipment/EditEquipment.jsx
+++ src/components/EditEquipment/EditEquipment.jsx
@@ -148,7 +148,6 @@ const EditEquipment = () => {
 
   return (
     <div>
-      <Navbar />
       <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 py-8 px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto">
           <button
